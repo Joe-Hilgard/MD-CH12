7f12b7d20f0afa2991b8ef993ee432e52afa9e0a
Joe Hilgard <jhilgard@gmail.com>
2015-04-24 13:57:26 -0500

    And next question.

--- ANOVA.r
+++ ANOVA.r
@@ -1,5 +1,5 @@
 library(reshape2) # tidyr is nicer but your laptops have too-old an R version
-
+library(lme4) # for mixed-effects models
 
 dat9 =read.table("chapter_12_exercise_9.dat")
 names(dat9) = c("LH-Grids", "RH-Grids", "LH-Brackets", "RH-Brackets") # name the conditions
@@ -10,24 +10,27 @@ dat9$Subject = 1:10 # add a column of subject numbers
 # Let's first pound the data flat.
   # in reshape2 the function is melt(),
   # in tidyr the function is gather()
-molten = melt(dat9, id.vars="Subject")
+molten9 = melt(dat9, id.vars="Subject")
 # Now our "variable" column really represents information we want in two columns.
   # One column for "hemisphere"
   # One column for grids/brackets.
 # If we had tidyr we could use separate() to tear "variable" into two columns.
 # Here we'll use grep().
-molten$hemisphere = NA
-molten$hemisphere[grep("LH", molten$variable)] = "left"
-molten$hemisphere[grep("RH", molten$variable)] = "right"
+molten9$hemisphere = NA
+molten9$hemisphere[grep("LH", molten9$variable)] = "left"
+molten9$hemisphere[grep("RH", molten9$variable)] = "right"
 # and again for grids
-molten$grids = NA
-molten$grids[grep("Grids", molten$variable)] = "grids"
-molten$grids[grep("Brackets", molten$variable)] = "brackets"
+molten9$grids = NA
+molten9$grids[grep("Grids", molten9$variable)] = "grids"
+molten9$grids[grep("Brackets", molten9$variable)] = "brackets"
 
 # okay! So now it's finally tidy!
 # Now we can run our model.
-model1 = aov(value ~ grids*hemisphere + Error(Subject/(hemisphere*grids)), data=molten)
+model1 = aov(value ~ grids*hemisphere + 
+               Error(Subject/(hemisphere*grids)), 
+             data=molten9)
 summary(model1)
+
 # d. 95% CI for condition effect just within the left hemisphere
   # filter the data just for hemisphere == left
 molten.LH = subset(molten, hemisphere == "left")
@@ -36,6 +39,22 @@ summary(model2)
 
 
 dat17=read.table("chapter_12_exercise_17.dat")
+# Add column names
+names(dat17) = c("Day1", "Day2", "Day3", "Day4", "Group")
+# Add column of subject numbers
+dat17$Subject = 1:14
+# This time when we melt, we want to keep "Group" as an ID variable
+# That is, we don't want to break it across rows.
+molten17 = melt(dat17, id.vars=c("Subject", "Group"))
+molten17
+# note what happens if we don't keep Group as an ID variable
+melt(dat17, id.vars=c("Subject"))
+# Now we can fit our ANOVA
+model17=aov(value ~ Group*variable + Error(Subject/variable),
+            dat = molten17)
+summary(model17)
+
+
 dat18=read.table("chapter_12_exercise_18.dat")
 dat19=read.table("chapter_12_exercise_19.dat")
 
